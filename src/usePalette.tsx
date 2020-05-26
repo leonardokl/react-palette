8f0821ae7c33938d5f73d8b0de3ec10fee1137e4
Leonardo Luiz <leonardokl@hotmail.com>
2020-05-26 12:12:27 -0300

    fix(usePalette): fix the returned type

--- src/usePalette.tsx
+++ src/usePalette.tsx
@@ -1,42 +1,41 @@
 import React from "react";
-const { useEffect, useReducer } = React;
 import { getPalette, PaletteColors } from "./getPalette";
 
 export type PaletteState = {
   loading: boolean;
   error?: Error;
   data: PaletteColors;
-}
+};
 
 const initialState: PaletteState = {
   loading: true,
   data: {},
-  error: undefined
-}
+  error: undefined,
+};
 
-function reducer(state: PaletteState, action) {
+function reducer(state: PaletteState, action): PaletteState {
   switch (action.type) {
-    case 'getPalette':
+    case "getPalette":
       return initialState;
-    case 'resolvePalette':
+    case "resolvePalette":
       return { ...state, data: action.payload, loading: false };
-    case 'rejectPalette':
+    case "rejectPalette":
       return { ...state, error: action.payload, loading: false };
   }
 }
 
 export function usePalette(src: string) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-  useEffect(() => {
-    dispatch({ type: 'getPalette' });
+  React.useEffect(() => {
+    dispatch({ type: "getPalette" });
 
     getPalette(src)
-      .then(palette => {
-        dispatch({ type: 'resolvePalette', payload: palette });
+      .then((palette) => {
+        dispatch({ type: "resolvePalette", payload: palette });
       })
-      .catch(ex => {
-        dispatch({ type: 'rejectPalette', payload: ex });
+      .catch((ex) => {
+        dispatch({ type: "rejectPalette", payload: ex });
       });
   }, [src]);
 
